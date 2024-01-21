e09e89af026e57c3379770c20718e6d12865848d
liangpatrick <liangpatrick1234@gmail.com>
2024-01-20 22:55:10 -0500

    Finished implementing searching for subsessions feature

--- lookup_agenda.py
+++ lookup_agenda.py
@@ -18,17 +18,35 @@ subsession_table = db_table(schemas.SUBSSESSION, schemas.SUBSSESSION_TABLE_SCHEM
 speaker_table = db_table(schemas.SPEAKER, schemas.SPEAKER_TABLE_SCHEMA)
 
 # If speaker is being searched, then need to search speaker table first to build new where parameter
-iniitial_results = []
+sessions = []
 if "speaker" in columns:
-    speakers = speaker_table.select([], where_param)
+    speakers = speaker_table.select(["session_id"], where_param)
     new_where_param = {}
-    for row in speakers:
-        if "session_id" not in new_where_param:
-            new_where_param["session_id"] = []
+    new_where_param["session_id"] = []
+    for row in speakers:            
         new_where_param["session_id"].append(row["session_id"])
-    iniitial_results = main_table.select([], new_where_param, "OR")
+    sessions = main_table.select([], new_where_param, "OR")
 else:
-    iniitial_results = main_table.select([], where_param)
+    sessions = main_table.select([], where_param)
+
+# Searching for subsessions
+session_ids_where_param = {}
+session_ids_where_param["session_id"] = []
+for row in sessions:
+    session_ids_where_param["session_id"].append(row["session_id"])
+subsessions = subsession_table.select(["subsession_id"], session_ids_where_param, "OR")
+
+# If subsessions exist, add to sessions
+if len(subsessions) > 0:
+    subsession_ids_where_param  = {}
+    subsession_ids_where_param["session_id"] = []
+    for row in subsessions:
+        subsession_ids_where_param["session_id"].append(row["subsession_id"])
+    sessions.extend(main_table.select([], subsession_ids_where_param, "OR"))
+
+# Printing sessions
+for session in sessions:
+    print(session + "\n\n")
 
 
 main_table.close()
